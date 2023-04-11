898f4f3e62a0b2ce9108657626b9cde5ff1e0fc9
Sinkyo <yhko1988@fastlane.kr>
2023-04-11 18:15:32 +0900

    feat : 라인 및 타이틀 애니메이션 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,17 @@ module.exports = {
         faktumTest: ["var(--font-faktumTest)"],
       },
       animation: {
-        showWebDescription: "showWebDescription 0.7s forwards 0.6s ease-in-out",
+        moveFromDownToUp0_7s:
+          "moveFromDownToUp0_7s 0.7s forwards 0.6s ease-in-out",
+        moveFromDownToUp0_6s:
+          "moveFromDownToUp0_7s 0.6s forwards 0.5s ease-in-out",
+        moveFromDownToUp0_5s:
+          "moveFromDownToUp0_7s 0.5s forwards 0.4s ease-in-out",
+        smoothUp: "smoothUp 0.7s forwards 0.6s ease-in-out",
+        lineAppear: "lineAppear 1s linear",
       },
       keyframes: {
-        showWebDescription: {
+        moveFromDownToUp0_7s: {
           "0%": {
             opacity: 0,
             transform: "translateY(150%)",
@@ -29,6 +36,38 @@ module.exports = {
           "100%": {
             opacity: 1,
             transform: "translateY(0%)",
+          },
+        },
+
+        smoothUp: {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(10%)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translateY(0%)",
+          },
+        },
+
+        lineAppear: {
+          "0%": {
+            height: "0%",
+          },
+          "5%": {
+            height: "5%",
+          },
+          "10%": {
+            height: "10%",
+          },
+          "20%": {
+            height: "20%",
+          },
+          "50%": {
+            height: "50%",
+          },
+          "100%": {
+            height: "100%",
           },
         },
       },
