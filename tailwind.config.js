5615ea47d2f0390da5251f15330aeb17e67b2750
Sinkyo <yhko1988@fastlane.kr>
2023-04-14 09:42:01 +0900

    fix : 애니메이션 요구 수정
    - 두번째 슬라이드에선 밑에서 위로
    
    - 첫번째에선 각기 따로 나오게

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,22 +18,23 @@ module.exports = {
         faktumTest: ["var(--font-faktumTest)"],
       },
       animation: {
-        moveFromUpToDownStartAfter0_5s:
-          "moveFromUpToDown 0.6s forwards 0.5s ease-in-out",
-        moveFromUpToDownStartAfter0_9s:
-          "moveFromUpToDown 0.6s forwards 0.9s ease-in-out",
-        moveFromUpToDownStartAfter1_1s:
-          "moveFromUpToDown 0.6s forwards 1.1s ease-in-out",
-        moveFromUpToDownStartAfter1_3s:
-          "moveFromUpToDown 0.6s forwards 1.3s ease-in-out",
-        smoothUp: "smoothUp 0.9s forwards 1s ease-in-out",
-        lineAppear: "lineAppear 0.6s backwards 0.7s linear",
+        moveFromDownToUpStartAfter0_5s:
+          "moveFromDownToUp 0.6s forwards 0.5s ease-in-out",
+        moveFromDownToUpStartAfter0_9s:
+          "moveFromDownToUp 0.6s forwards 0.9s ease-in-out",
+        moveFromDownToUpStartAfter1_1s:
+          "moveFromDownToUp 0.6s forwards 1.1s ease-in-out",
+        moveFromDownToUpStartAfter1_3s:
+          "moveFromDownToUp 0.6s forwards 1.3s ease-in-out",
+        smoothUpStartFirst: "smoothUp 0.7s forwards 0.5s linear",
+        smoothUpStartSecond: "smoothUp 0.7s forwards 0.9s linear",
+        lineAppear: "lineAppear 0.6s reverse backwards 0.7s linear",
       },
       keyframes: {
-        moveFromUpToDown: {
+        moveFromDownToUp: {
           "0%": {
             opacity: 0,
-            transform: "translateY(-70%)",
+            transform: "translateY(70%)",
           },
           "100%": {
             opacity: 1,
@@ -44,20 +45,20 @@ module.exports = {
         smoothUp: {
           "0%": {
             opacity: 0,
-            transform: "translateY(10%)",
+            transform: "translateY(20px)",
           },
           "100%": {
             opacity: 1,
-            transform: "translateY(0%)",
+            transform: "translateY(0px)",
           },
         },
 
         lineAppear: {
-          "0%": {
+          "100%": {
             height: "0%",
           },
 
-          "100%": {
+          "0%": {
             height: "100%",
           },
         },
