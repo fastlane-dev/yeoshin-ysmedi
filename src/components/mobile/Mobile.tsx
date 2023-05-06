13b9fb3ffbb5d593b4e7b4b17bdb7b36b0a87ae9
Sinkyo <yhko1988@fastlane.kr>
2023-05-06 09:53:02 +0900

    refactor : Mobile컴포넌트 barrel모드로 import

--- src/components/mobile/Mobile.tsx
+++ src/components/mobile/Mobile.tsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import { Mousewheel, Pagination } from "swiper";
 
-export const Mobile = () => {
+const Mobile = () => {
   return (
     <>
       <Swiper
@@ -16,20 +16,20 @@ export const Mobile = () => {
           clickable: true,
         }}
         modules={[Mousewheel]}
-        className="mySwiper swiper-mobile"
+        className="mySwiper"
         speed={800}
       >
-        <SwiperSlide className="swiper-slide-mobile">
+        <SwiperSlide>
           <div className="relative h-screen w-screen  font-faktumTest text-[30px] text-white">
             slide 1
           </div>
         </SwiperSlide>
-        <SwiperSlide className="swiper-slide-mobile">
+        <SwiperSlide>
           <div className="relative h-screen w-screen  font-faktumTest text-[30px] text-white">
             slide 2
           </div>
         </SwiperSlide>
-        <SwiperSlide className="swiper-slide-mobile">
+        <SwiperSlide>
           <div className="relative h-screen w-screen  font-faktumTest text-[30px] text-white">
             slide 3
           </div>
@@ -38,3 +38,73 @@ export const Mobile = () => {
     </>
   );
 };
+
+export default Mobile;
+
+// const Container = ({ number }) => {
+//   const swiper = useSwiper();
+//   const swiperSlide = useSwiperSlide();
+
+//   if (swiperSlide.isActive) {
+//     console.log(swiper.activeIndex);
+//   }
+//   return <div>{number}</div>;
+// };
+
+// export default function App() {
+//   const [controlledSwiper, setControlledSwiper] = useState(null);
+//   return (
+//     <>
+//       <Swiper
+//         className="mySwiper swiper-h"
+//         spaceBetween={50}
+//         pagination={{
+//           clickable: true,
+//         }}
+//         modules={[Controller]}
+//         direction={"vertical"}
+//         controller={{ control: controlledSwiper }}
+//       >
+//         <SwiperSlide>
+//           <Container number={"가"} />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <Container number={"나"} />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <Swiper
+//             direction={"vertical"}
+//             spaceBetween={50}
+//             slidesPerView={"auto"}
+//             freeMode={true}
+//             scrollbar={true}
+//             mousewheel={true}
+//             modules={[FreeMode, Scrollbar, Mousewheel]}
+//           >
+//             <SwiperSlide>
+//               <Container number={1} />{" "}
+//             </SwiperSlide>
+//             <SwiperSlide>
+//               <Container number={2} />{" "}
+//             </SwiperSlide>
+//             <SwiperSlide>
+//               <Container number={3} />{" "}
+//             </SwiperSlide>
+//             <SwiperSlide>
+//               <Container number={4} />{" "}
+//             </SwiperSlide>
+//             <SwiperSlide>
+//               <Container number={5} />{" "}
+//             </SwiperSlide>
+//           </Swiper>
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <Container number={"다"} />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <Container number={"라"} />
+//         </SwiperSlide>
+//       </Swiper>
+//     </>
+//   );
+// }
