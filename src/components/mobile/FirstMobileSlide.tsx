eb0f62663180728f55fa65ca71366fedf51959dc
Sinkyo <yhko1988@fastlane.kr>
2023-05-06 13:32:54 +0900

    feat : 모바일 버전 첫번쨰 슬라이드 완성

--- src/components/mobile/FirstMobileSlide.tsx
+++ src/components/mobile/FirstMobileSlide.tsx
@@ -1,39 +1,28 @@
 import Image from "next/image";
 import React from "react";
-import planetImg from "../../../public/img/planet.png";
+import mobilePlanet from "../../../public/img/mobile_planet.svg";
 
 export const FirstMobileSlide = () => {
   return (
     <section className="relative h-screen w-screen  font-faktumTest text-[30px] text-white">
-      <div className="absolute bottom-[68px] left-[373px] z-[-1] opacity-[0.7]">
-        <Image src={planetImg} alt="planet" priority />
+      <div className="absolute top-[-22%] z-[-1] h-[82vh] w-[152vw] rotate-[5.7deg] opacity-[0.7]">
+        <Image
+          className="absolute"
+          src={mobilePlanet}
+          alt="planet"
+          fill
+          priority
+        />
       </div>
-      <div className="flex-center w-full justify-between px-[160px] pt-[80px]">
-        <div>
-          <Image src={"/img/logo.png"} alt="logo" width={189} height={35} />
-        </div>
-        <nav className="flex-center cursor-pointer gap-[41px]">
-          <a href="https://mall.ysmedi.kr/" target="_blank">
-            <Image
-              src={"/img/shopping_mall.png"}
-              alt="shopping_mall"
-              width={179}
-              height={36}
-            />
-          </a>
-          <div className="flex-center gap-[10px] text-[20px] font-[200]">
-            <span>KR</span>
-            <span className="text-[#888888]">|</span>
-            <span className="text-[#888888]">EN</span>
-          </div>
-        </nav>
+      <div className="pl-[31px] pt-[44px]">
+        <Image src={"/img/logo.png"} alt="logo" width={108} height={20} />
       </div>
-      <div className="flex-center-column absolute bottom-[317px] left-[160px]  items-start gap-[40px]">
-        <p className=" animate-smoothUpStartFirst whitespace-pre-wrap text-[100px] font-[400] leading-[100px] tracking-[-0.02em]  opacity-0">
+      <div className="flex-center-column ml-[23px] mt-[50vh] items-start gap-[2.7vh] mmd:mt-[35vh] mmd:gap-[6vh]">
+        <p className=" animate-smoothUpStartFirst text-[13vw] font-[400] leading-[7vh] tracking-[-0.02em]  opacity-0 msm:leading-[8.5vh]  mmd:leading-[12vh] mlg:leading-[15vh]">
           {"Your beauty,\nour priority"}
         </p>
-        <p className="animate-smoothUpStartSecond whitespace-pre-wrap font-pretendard text-[26px] font-[100] leading-[33px] tracking-[-0.02em]  opacity-0">
-          {"우리는 당신의 아름다움을 극대화하는\n방법을 연구합니다"}
+        <p className="animate-smoothUpStartSecond font-pretendard text-[3.7vw] font-[100]  tracking-[-0.02em]  opacity-0">
+          {"우리는 당신의 아름다움을 극대화하는 방법을 연구합니다"}
         </p>
       </div>
     </section>
