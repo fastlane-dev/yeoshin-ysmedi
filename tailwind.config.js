b18438e617e9ac7e73835c39d8ffa58559b11674
Sinkyo <yhko1988@fastlane.kr>
2023-05-06 17:15:15 +0900

    feat : 모바일에 맞게 애니메이션 속도 조정

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,12 @@ module.exports = {
         faktumTest: ["var(--font-faktumTest)"],
       },
       animation: {
+        mobileMoveFromDownToUpStartAfter0_6s:
+          "moveFromDownToUp 0.6s forwards 0.6s ease-in-out",
+        mobileMoveFromDownToUpStartAfter0_8s:
+          "moveFromDownToUp 0.6s forwards 0.8s ease-in-out",
+        mobileMoveFromDownToUpStartAfter1s:
+          "moveFromDownToUp 0.6s forwards 1s ease-in-out",
         moveFromDownToUpStartAfter0_5s:
           "moveFromDownToUp 0.6s forwards 0.5s ease-in-out",
         moveFromDownToUpStartAfter0_9s:
@@ -34,6 +40,10 @@ module.exports = {
         smoothUpStartFirst: "smoothUp 0.7s forwards 0.5s ease-in-out",
         smoothUpStartSecond: "smoothUp 0.7s forwards 0.9s ease-in-out",
         lineAppear: "lineAppear 0.6s backwards 0.7s linear",
+
+        wholeSectionAppear: "opacityAppear 0.6s forwards linear",
+
+        mobileLineAppear: "mobileLineAppear 0.6s forwards 1.3s linear",
       },
       keyframes: {
         moveFromDownToUp: {
@@ -44,6 +54,15 @@ module.exports = {
           "100%": {
             opacity: 1,
             transform: "translateY(0%)",
+          },
+        },
+
+        opacityAppear: {
+          "0%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 1,
           },
         },
 
@@ -65,6 +84,16 @@ module.exports = {
 
           "100%": {
             height: "100%",
+          },
+        },
+
+        mobileLineAppear: {
+          "0%": {
+            width: "0%",
+          },
+
+          "100%": {
+            width: "100%",
           },
         },
       },
