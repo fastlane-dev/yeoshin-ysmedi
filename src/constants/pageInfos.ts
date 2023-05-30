f39a6313c9bfa565d3b1ae66ff08396b4928ff62
Sinkyo <yhko1988@fastlane.kr>
2023-05-30 17:12:13 +0900

    feat : 랜더링되는 컴포넌트 정보를 한 곳에 모음

---
+++ src/constants/pageInfos.ts
@@ -0,0 +1,25 @@
+import YsMedi from "@/components/Ysmedi";
+
+export const PAGE_IDENTITY = {
+  YS_MEDI: "ysmedi",
+  LALA_PEEL: "lhalala",
+  THERFECT: "therfect",
+} as const;
+
+export const PAGE_INFOS = {
+  [PAGE_IDENTITY.YS_MEDI]: {
+    title: "YS MEDI",
+    Component: YsMedi,
+    faviconPath: "/ys_medi_favicon.ico",
+  },
+  [PAGE_IDENTITY.LALA_PEEL]: {
+    title: "LHALALA",
+    Component: YsMedi,
+    faviconPath: "",
+  },
+  [PAGE_IDENTITY.THERFECT]: {
+    title: "Therfect",
+    Component: YsMedi,
+    faviconPath: "",
+  },
+};
