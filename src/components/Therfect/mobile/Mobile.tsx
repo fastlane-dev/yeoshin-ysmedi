a7a4d491d7e6c829eaeb080bf30e377b206ba9fa
Sinkyo <yhko1988@fastlane.kr>
2023-06-12 19:04:01 +0900

    feat : swiper 사용하지 않고 직접 슬라이드 구현

--- src/components/Therfect/mobile/Mobile.tsx
+++ src/components/Therfect/mobile/Mobile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useRef, useState } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -16,6 +16,9 @@ import { NinethMobileSlide } from "./NinethMobileSlide";
 import { changeBodyBackground } from "../common/changeBodyBackground";
 
 const Mobile = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const firstRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     changeBodyBackground("#f68e0f");
   }, []);
@@ -24,84 +27,30 @@ const Mobile = () => {
   useEffect(() => {
     setInnerHeight(window.innerHeight);
   }, []);
-  return (
-    <Swiper
-      direction={"vertical"}
-      slidesPerView={1}
-      mousewheel={true}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Mousewheel]}
-      className="mySwiper"
-      speed={800}
-      height={innerHeight}
-      onSlideChangeTransitionEnd={(swiper) => {
-        // NOTE: 아이폰에서 슬라이드 할때마다 배경을 바꿔주어야 함
-        switch (swiper.activeIndex) {
-          case 0:
-            changeBodyBackground("#f68e0f");
-            break;
-          case 1:
-          case 7:
-            changeBodyBackground("white");
-            break;
-          case 2:
-          case 6:
-            changeBodyBackground("#F9F9F9");
-            break;
-          case 3:
-            changeBodyBackground("#ecddd7");
-            break;
-          case 4:
-          case 5:
-            changeBodyBackground("black");
-            break;
 
-          default:
-            break;
-        }
+  console.log(containerRef.current);
+
+  return (
+    <div
+      ref={containerRef}
+      className="overflow-auto"
+      onScroll={(event) => {
+        console.log(event);
+      }}
+      style={{
+        height: innerHeight,
       }}
     >
-      <SwiperSlide>
-        <FirstMobileSlide innerHeight={innerHeight} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <SecondMobileSlide innerHeight={innerHeight} />
-      </SwiperSlide>
-      <SwiperSlide style={{ height: "20dvh" }}>
-        <Swiper
-          className="bg-[#F9F9F9]"
-          direction={"vertical"}
-          freeMode={true}
-          scrollbar={true}
-          mousewheel={true}
-          modules={[FreeMode, Scrollbar]}
-        >
-          <SwiperSlide>
-            <ThirdMobileSlide />
-          </SwiperSlide>
-          <SwiperSlide className="bg-[#F9F9F9]">
-            <FourthMobileSlide />
-          </SwiperSlide>
-        </Swiper>
-      </SwiperSlide>
-      <SwiperSlide>
-        <FifthMobileSlide innerHeight={innerHeight} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <SixthMobileSlide innerHeight={innerHeight} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <SeventhMobileSlide innerHeight={innerHeight} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <EighthMobileSlide innerHeight={innerHeight} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <NinethMobileSlide innerHeight={innerHeight} />
-      </SwiperSlide>
-    </Swiper>
+      <FirstMobileSlide ref={firstRef} innerHeight={innerHeight} />
+      <SecondMobileSlide innerHeight={innerHeight} />
+      <ThirdMobileSlide />
+      <FourthMobileSlide />
+      <FifthMobileSlide innerHeight={innerHeight} />
+      <SixthMobileSlide innerHeight={innerHeight} />
+      <SeventhMobileSlide innerHeight={innerHeight} />
+      <EighthMobileSlide innerHeight={innerHeight} />
+      <NinethMobileSlide innerHeight={innerHeight} />
+    </div>
   );
 };
 
