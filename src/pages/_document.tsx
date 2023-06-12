4a44c141f7fe8dfb6aa0c79480a0ea38ce01b471
Sinkyo <yhko1988@fastlane.kr>
2023-06-12 17:26:07 +0900

    feat : 테스트로 리모트 디버깅

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          data-consolejs-channel="d4977db3-0a21-2b2a-2150-ce85031da747"
+          src="https://remotejs.com/agent/agent.js"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
