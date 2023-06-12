f9fc78670a9efb12b12c517c7701821d3280f703
Sinkyo <yhko1988@fastlane.kr>
2023-06-12 19:04:26 +0900

    feat : remote debugging 실험 종료

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          data-consolejs-channel="d4977db3-0a21-2b2a-2150-ce85031da747"
-          src="https://remotejs.com/agent/agent.js"
-        ></script>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
