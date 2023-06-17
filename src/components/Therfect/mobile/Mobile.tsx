d0ca6d1686797c5233e54a3c0dbb6a37ecff81c4
Sinkyo <yhko1988@fastlane.kr>
2023-06-17 15:48:09 +0900

    fix : 모든 슬라이드의 높이를 dvh로 설정하고 auto height 추가
    - 노즐 슬라이드 여백 문제 해결됨

--- src/components/Therfect/mobile/Mobile.tsx
+++ src/components/Therfect/mobile/Mobile.tsx
@@ -1,4 +1,8 @@
-import React, { forwardRef, useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { FreeMode, Mousewheel, Scrollbar } from "swiper";
 
 import { FirstMobileSlide } from "./FirstMobileSlide";
 import { SecondMobileSlide } from "./SecondMobileSlide";
@@ -11,98 +15,79 @@ import { EighthMobileSlide } from "./EighthMobileSlide";
 import { NinethMobileSlide } from "./NinethMobileSlide";
 import { changeBodyBackground } from "../common/changeBodyBackground";
 
-const NozzleContainer = forwardRef<HTMLElement>((_, ref) => (
-  <section ref={ref}>
-    <ThirdMobileSlide />
-    <FourthMobileSlide />
-  </section>
-));
-
 const Mobile = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  // const children = useRef(Array.from({length:9},()=><></>));
-  const children = useRef<(HTMLElement | null)[]>([]);
-  const [childrenEle, setChildrenEle] = useState<HTMLElement[]>([]);
-
-  useEffect(() => {
-    changeBodyBackground("#f68e0f");
-  }, []);
-
-  const [innerHeight, setInnerHeight] = useState(0);
-
-  useEffect(() => {
-    setInnerHeight(window.innerHeight);
-    // containerRef?.current?.scroll({
-    //   behavior: "smooth",
-    //   top: innerHeight * 2 + 994.6,
-    // });
-  }, []);
-
-  const pushChildren = (el: HTMLElement) => {
-    children.current.push(el);
-  };
-
   return (
-    <div
-      ref={containerRef}
-      className="overflow-auto"
-      onScroll={(event) => {
-        // TODO: 한곳에 함수로 빼두기
-        const { bottom: firstEleBottom, height: firstEleHeight } = (
-          children.current[0] as HTMLElement
-        ).getBoundingClientRect();
-        const { bottom: secondEleBottom, height: secondEleHeight } = (
-          children.current[1] as HTMLElement
-        ).getBoundingClientRect();
-        const { bottom: thirdEleBottom, height: thirdleHeight } = (
-          children.current[2] as HTMLElement
-        ).getBoundingClientRect();
-        const { bottom: fourthEleBottom, height: fourthEleHeight } = (
-          children.current[3] as HTMLElement
-        ).getBoundingClientRect();
-        const { bottom: fifthEleBottom, height: fifthEleHeight } = (
-          children.current[4] as HTMLElement
-        ).getBoundingClientRect();
-        const { bottom: sixthEleBottom, height: sixthEleHeight } = (
-          children.current[5] as HTMLElement
-        ).getBoundingClientRect();
-        const { bottom: seventhEleBottom, height: seventhEleHeight } = (
-          children.current[6] as HTMLElement
-        ).getBoundingClientRect();
-        const { bottom: eighthEleBottom, height: eighthEleHeight } = (
-          children.current[6] as HTMLElement
-        ).getBoundingClientRect();
+    <Swiper
+      direction={"vertical"}
+      mousewheel={true}
+      modules={[Mousewheel]}
+      className="mySwiper"
+      speed={800}
+      autoHeight
+      onSlideChangeTransitionEnd={(swiper) => {
+        switch (swiper.activeIndex) {
+          case 0:
+            changeBodyBackground("#f68e0f");
+            break;
+          case 1:
+          case 7:
+            changeBodyBackground("white");
+            break;
+          case 2:
+          case 6:
+            changeBodyBackground("#F9F9F9");
+            break;
+          case 3:
+            changeBodyBackground("#ecddd7");
+            break;
+          case 4:
+          case 5:
+            changeBodyBackground("black");
+            break;
 
-        // NOTE: 위에서 밑으로 / 밑에서 위로
-        // NOTE: 노즐 컨테이너를 지날때는 scroll 함수가 실행되면 안됨.
-
-        // const isFirstElementSlidedToBottom =
-        // const isFirstElementSlidedToTop =
-        // const isSecondElementSlidedToBottom =
-        // const isSecondElementSlidedToBottom =
-        // const isThirdElementSlidedToBottom = thirdElementHeight
-        // console.log("thirdElementBottomPos", thirdElementBottomPos);
-        console.log("thirdele", children.current[2]?.getBoundingClientRect());
-
-        // console.log("secondele", children.current[2]?.getBoundingClientRect());
-
-        // console.log("event", event);
-        // console.log(containerRef.current?.getBoundingClientRect().top);
-        // console.log(children.current[2]?.getBoundingClientRect());
-      }}
-      style={{
-        height: innerHeight,
+          default:
+            break;
+        }
       }}
     >
-      <FirstMobileSlide ref={pushChildren} innerHeight={innerHeight} />
-      <SecondMobileSlide ref={pushChildren} innerHeight={innerHeight} />
-      <NozzleContainer ref={pushChildren} />
-      <FifthMobileSlide ref={pushChildren} innerHeight={innerHeight} />
-      <SixthMobileSlide ref={pushChildren} innerHeight={innerHeight} />
-      <SeventhMobileSlide ref={pushChildren} innerHeight={innerHeight} />
-      <EighthMobileSlide ref={pushChildren} innerHeight={innerHeight} />
-      <NinethMobileSlide ref={pushChildren} innerHeight={innerHeight} />
-    </div>
+      <SwiperSlide>
+        <FirstMobileSlide />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SecondMobileSlide />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Swiper
+          className="process-slider bg-[#F9F9F9] "
+          direction={"vertical"}
+          freeMode={true}
+          modules={[FreeMode, Scrollbar]}
+          slidesPerView="auto"
+        >
+          <SwiperSlide>
+            <ThirdMobileSlide />
+          </SwiperSlide>
+          <SwiperSlide className="bg-[#F9F9F9]">
+            <FourthMobileSlide />
+          </SwiperSlide>
+        </Swiper>
+      </SwiperSlide>
+      <SwiperSlide>
+        <FifthMobileSlide />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SixthMobileSlide />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SeventhMobileSlide />
+      </SwiperSlide>
+      <SwiperSlide>
+        <EighthMobileSlide />
+      </SwiperSlide>
+      <SwiperSlide>
+        <NinethMobileSlide />
+      </SwiperSlide>
+    </Swiper>
   );
 };
 
