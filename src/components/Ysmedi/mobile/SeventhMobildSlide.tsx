8b376cdc05d4c19432c79a94569a8acae8371d28
Sinkyo <yhko1988@fastlane.kr>
2023-06-19 15:10:23 +0900

    fix : 와이에스 메디 모바일 이미지 늘어나는 이슈 해결
    - 아예 바뀐 이미지를 새로 교체함(가로보다 세로가 긴)

--- src/components/Ysmedi/mobile/SeventhMobildSlide.tsx
+++ src/components/Ysmedi/mobile/SeventhMobildSlide.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-
+// cdn에 이미지 올라가면 이걸로 교체. src={`${process.env.NEXT_PUBLIC_CDN_IMAGES}ysmedi/lala_background.png`}
 export const SeventhMobildSlide = () => {
   return (
-    <section className="flex-center relative h-screen w-screen items-end font-faktumTest tracking-[-0.02em] text-white">
-      <div className="absolute h-full w-full">
+    <section className="flex-center relative h-screen w-screen items-end overflow-hidden font-faktumTest tracking-[-0.02em] text-white">
+      <div className="absolute h-screen w-screen">
         <img
-          src={`${process.env.NEXT_PUBLIC_CDN_IMAGES}ysmedi/lala_background.png`}
+          src={"/img/ysmedi/lala_background.png"}
           alt="lala_background"
-          className="absolute bottom-0 h-screen"
+          className="absolute object-contain"
         />
       </div>
       <div className="flex-center-column  absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] items-center gap-[21px]">
--- src/components/Ysmedi/mobile/SixthMobildSlide.tsx
+++ src/components/Ysmedi/mobile/SixthMobildSlide.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
+// cdn에 이미지 올라가면 이걸로 교체. src={`${process.env.NEXT_PUBLIC_CDN_IMAGES}ysmedi/therfect_background.png`}
 export const SixthMobildSlide = () => {
   return (
-    <section className="flex-center relative h-screen w-screen items-end font-faktumTest tracking-[-0.02em] text-white">
+    <section className="flex-center relative h-screen w-screen items-end overflow-hidden font-faktumTest tracking-[-0.02em] text-white">
       <div className="absolute h-screen w-screen">
         <img
-          src={`${process.env.NEXT_PUBLIC_CDN_IMAGES}ysmedi/therfect_background.png`}
+          src={"/img/ysmedi/therfect_background.png"}
           alt="therfect_backgrond"
-          className="absolute bottom-0 h-[80dvh]"
+          className="absolute bottom-0 object-contain"
         />
       </div>
       <div className="flex-center-column  absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] items-center gap-[21px]">
