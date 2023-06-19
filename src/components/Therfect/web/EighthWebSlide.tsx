b43bf98c1d2e7610ba6189a5747575a34b910b7b
Sinkyo <yhko1988@fastlane.kr>
2023-06-19 14:53:19 +0900

    fix : 써팩트 웹 마지막 슬라이드, 회색 선 분리 성공

--- src/components/Therfect/web/EighthWebSlide.tsx
+++ src/components/Therfect/web/EighthWebSlide.tsx
@@ -2,19 +2,21 @@ import React from "react";
 
 export const EighthWebSlide = () => {
   return (
-    <footer className="relative h-screen w-screen max-w-[1920px] bg-white font-faktumTest font-[400]">
-      <div>
-        <div className="mx-[150px] mb-[72px] pt-[112px] font-pretendard text-[20px] font-[300] leading-[36px] text-[#555555] sh:mb-[18px] sh:pt-[18px] sh:text-[1dvw]">
-          <p className="mb-[36px]">{"지금 바로 병원에서 만나보세요."}</p>
-          <p>
-            {
-              "Therfect™, 써펙트™는 YSmedi의 상표로 이는 권리자의 지적재산권에 해당하며, 무단으로 사용하는 경우 상표권 침해행위로 간주되며, 상표 유사문구를 무단으로 사용하는 것은 ‘부정경쟁방지 및\n영업비밀보호에 관한 법률’상의 부정경쟁 행위에 해당합니다.\nTherfect™, 써펙트™는 식약처 허가를 받은 의료기기입니다.\n범용전기수술기로, 고주파 전류를 사용하여 조직의 응고에 사용하는 기구입니다. 이 제품은 ‘의료기기’이며, ‘사용상의 주의사항’과 '사용방법'을 잘 읽고 사용하십시오."
-            }
-          </p>
+    <section className="flex w-screen flex-col items-center">
+      <section className="relative w-screen max-w-[1920px] bg-white font-faktumTest font-[400]">
+        <div>
+          <div className="mx-[150px] mb-[72px] pt-[112px] font-pretendard text-[20px] font-[300] leading-[36px] text-[#555555] sh:mb-[18px] sh:pt-[18px] sh:text-[1dvw]">
+            <p className="mb-[36px]">{"지금 바로 병원에서 만나보세요."}</p>
+            <p>
+              {
+                "Therfect™, 써펙트™는 YSmedi의 상표로 이는 권리자의 지적재산권에 해당하며, 무단으로 사용하는 경우 상표권 침해행위로 간주되며, 상표 유사문구를 무단으로 사용하는 것은 ‘부정경쟁방지 및\n영업비밀보호에 관한 법률’상의 부정경쟁 행위에 해당합니다.\nTherfect™, 써펙트™는 식약처 허가를 받은 의료기기입니다.\n범용전기수술기로, 고주파 전류를 사용하여 조직의 응고에 사용하는 기구입니다. 이 제품은 ‘의료기기’이며, ‘사용상의 주의사항’과 '사용방법'을 잘 읽고 사용하십시오."
+              }
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="mb-[110px] w-full border-t-[1px] border-[#C0C0C0] sh:mb-[35px]  xxlg:ml-[148px] xxlg:w-[84%]"></div>
-      <div className="flex h-[110px] w-full justify-between px-[165px]">
+      </section>
+      <div className=" mb-[110px] w-full  border-t-[1px] border-[#C0C0C0] sh:mb-[35px]"></div>
+      <section className="flex h-[110px] w-full max-w-[1920px] justify-between px-[165px]">
         <div className="flex-center-column items-start gap-[30px] text-[18px] leading-[24px] sh:text-[1dvw]">
           <div className="flex-center gap-[50px] font-[200]">
             <p>TEL. 070-4858-4686</p>
@@ -35,7 +37,7 @@ export const EighthWebSlide = () => {
             className="h-[41px] w-[221px]"
           />
         </div>
-      </div>
-    </footer>
+      </section>
+    </section>
   );
 };
