a5d10d29ffdd1d9c56ba382b8961a5ae144b5a28
Sinkyo <yhko1988@fastlane.kr>
2023-06-23 16:40:12 +0900

    feat : google analytics 적용 완료

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
 import type { AppProps } from "next/app";
 import "swiper/css";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':newDate().getTime(),event:'gtm.js'});
+              varf=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})
+              (window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');`,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
