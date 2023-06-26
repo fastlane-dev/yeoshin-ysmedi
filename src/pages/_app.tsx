37c45116f9d11e19dffb328ea4c4fa20fbcb72e5
Sinkyo <yhko1988@fastlane.kr>
2023-06-26 18:25:03 +0900

    feat : gtag를 app에 일단 적용
    - document에서는 선택 적용을 할 수 없음(써팩트만 적용)
    - 조회수 유입 감지

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,24 +3,79 @@ import "swiper/css";
 import "@/styles/globals.css";
 import Head from "next/head";
 import { NextSeo } from "next-seo";
+import { PAGE_IDENTITY } from "@/constants/pageInfos";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import * as gtag from "@/lib/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { seoData } = pageProps;
+  const { seoData, pageIdentity } = pageProps;
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
       <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':newDate().getTime(),event:'gtm.js'});
+        {pageIdentity === PAGE_IDENTITY.THERFECT ? (
+          <>
+            {/* <!--Googletag(gtag.js)--> */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+  window.dataLayer=window.dataLayer||[];
+  functiongtag(){dataLayer.push(arguments);}
+  gtag('js',newDate());gtag('config','${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
+  `,
+              }}
+            />
+          </>
+        ) : null}
+        {pageIdentity === PAGE_IDENTITY.THERFECT ? (
+          <>
+            {/* <!--GoogleTagManager--> */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':newDate().getTime(),event:'gtm.js'});
               varf=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
               j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})
               (window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');`,
-          }}
-        />
+              }}
+            />
+            {/* <!--EndGoogleTagManager--> */}
+          </>
+        ) : null}
         {seoData && <meta name="keywords" content={seoData.keywords} />}
       </Head>
       {seoData ? <NextSeo {...seoData} /> : <></>}
+      {pageIdentity === PAGE_IDENTITY.THERFECT ? (
+        <>
+          {/* <!--GoogleTagManager(noscript)--> */}
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+          {/* <!--EndGoogleTagManager(noscript)--> */}
+        </>
+      ) : null}
       <Component {...pageProps} />;
     </>
   );
