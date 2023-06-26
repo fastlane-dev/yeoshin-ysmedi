ba9ec23d527eff17429d2995a21766814a428b47
Sinkyo <yhko1988@fastlane.kr>
2023-06-26 16:49:07 +0900

    refactor : seoMapper에서 title 설정하기 때문

--- src/constants/pageInfos.ts
+++ src/constants/pageInfos.ts
@@ -10,17 +10,14 @@ export const PAGE_IDENTITY = {
 
 export const PAGE_INFOS = {
   [PAGE_IDENTITY.YS_MEDI]: {
-    title: "YS MEDI",
     Component: YsMedi,
     faviconPath: "/ys_medi_favicon.ico",
   },
   [PAGE_IDENTITY.LALA_PEEL]: {
-    title: "LHALALA",
     Component: LalaPeel,
     faviconPath: "/lhalala_peel_favicon.ico",
   },
   [PAGE_IDENTITY.THERFECT]: {
-    title: "Therfect",
     Component: Therfect,
     faviconPath: "/therfect_favicon.ico",
   },
