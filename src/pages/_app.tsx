e855d71cf5d83855e65041b364d805b8b1f9fe8f
Sinkyo <yhko1988@fastlane.kr>
2023-06-26 19:08:30 +0900

    fix : lib 경로 바꿔서 다시 실행

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { PAGE_IDENTITY } from "@/constants/pageInfos";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-import * as gtag from "@/lib/gtag";
+import * as gtag from "../../lib/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { seoData, pageIdentity } = pageProps;
