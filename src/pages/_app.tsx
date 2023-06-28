4d3c69c7948b22a0fb34b62485a2e63ddf3c20e3
Sinkyo <yhko1988@fastlane.kr>
2023-06-28 19:27:10 +0900

    fix : gtag 수정
    - 띄어쓰기 안해서 그런것임
    
    - 써팩트일때만 gtag가 심어지도록

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,20 +27,55 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        {/* <!--GoogleTagManager--> */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':newDate().getTime(),event:'gtm.js'});
+        {pageIdentity === PAGE_IDENTITY.THERFECT ? (
+          <>
+            {/* <!--Googletag(gtag.js)--> */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+  window.dataLayer=window.dataLayer||[];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js',new Date());
+  gtag('config','${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
+  `,
+              }}
+            />
+
+            {/* <!--GoogleTagManager--> */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
           varf=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
           j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})
           (window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');`,
-          }}
-        />
-        {/* <!--EndGoogleTagManager--> */}
+              }}
+            />
+            {/* <!--EndGoogleTagManager--> */}
+          </>
+        ) : null}
+
         {seoData && <meta name="keywords" content={seoData.keywords} />}
       </Head>
       {seoData ? <NextSeo {...seoData} /> : <></>}
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
+
+      {pageIdentity === PAGE_IDENTITY.THERFECT ? (
+        <>
+          {/* <!--GoogleTagManager(noscript)--> */}
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+        </>
+      ) : null}
     </>
   );
 }
